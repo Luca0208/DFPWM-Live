554786dd4b3148ca2cfd75b983fe61886d607e73
TheJebForge <jeb@thejebforge.com>
2020-08-13 10:53:21 +0300

    More PR fixes

--- Backend/stream_localaudio.sh
+++ Backend/stream_localaudio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Usage: ./stream_mp3local.sh <WSPort> <PathToMP3>
+# Usage: ./stream_mp3local.sh <WSPort> <PathToAudioFile>
 
 sox $2 -traw -e signed-integer -b 8 -c 1 -r 48000 - | ./aucmp | wscat -b -l $1

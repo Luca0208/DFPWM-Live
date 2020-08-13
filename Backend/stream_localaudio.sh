daf42ab98cb79329d86fed34fef191f1198ce737
Luca <luca@acul.me>
2020-08-13 10:15:33 +0200

    Fix up the shell scripts a bit

--- Backend/stream_localaudio.sh
+++ Backend/stream_localaudio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Usage: ./stream_localaudio.sh <WSPort> <PathToAudioFile>
 
-sox $2 -traw -e signed-integer -b 8 -c 1 -r 48000 - | ./aucmp | wscat -b -l $1
+sox "$2" -traw -e signed-integer -b 8 -c 1 -r 48000 - | ./aucmp | wscat -b -l $1
--- Backend/stream_mp3url.sh
+++ Backend/stream_mp3url.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Usage: ./stream_mp3url.sh <WSPort> <URL>
 
-curl --output - -L $2| sox -tmp3 - -traw -e signed-integer -b 8 -c 1 -r 96000 - | ./aucmp | wscat -b -l $1
+curl --output - -L "$2" | sox -tmp3 - -traw -e signed-integer -b 8 -c 1 -r 96000 - | ./aucmp | wscat -b -l $1

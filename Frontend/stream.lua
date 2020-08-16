29667b9a6799acfcc8222048bb7cec081a9b1fda
Luca <luca@acul.me>
2020-08-17 00:31:03 +0200

    Add more statistics

--- Frontend/stream.lua
+++ Frontend/stream.lua
@@ -54,6 +54,7 @@ local tape = setmetatable({}, {
 local running = true
 local wss
 local total = 0
+local started = os.clock()
 local startedPlaying = os.clock() + 5
 local lastWrite = os.clock()
 local dlSpeed = 0
@@ -98,7 +99,11 @@ end
 local handler = {
     ["websocket_success"] = function(url, ws)
         if url == wsurl then
-            print("Connected!")
+            print("Thanks for using DFPWM-Live!")
+            print("Playtime |  DL   |DL Total |Periph. Calls |")
+            print("---------|-------|---------|------|-------|")
+            startTimer = os.startTimer(5)
+            repeatingTimer = os.startTimer(1)
             wss = ws
         end
     end,
@@ -147,7 +152,13 @@ local handler = {
 
             local formattedTime = ("%2d:%2d:%2d"):format(hours, minutes, seconds):gsub(" ", "0")
 
-            term.write(("%s %s %9s"):format(formattedTime, format(dlSpeed, 1, true) .. "B/s", format(total, 2, true) .. "B"))
+            term.write(("%s|%7s|%9s|%6d|%7s"):format(
+                formattedTime,
+                format(dlSpeed, 0, true) .. "B/s",
+                format(total, 2, true) .. "B"),
+                format(peripheralCalls, 2),
+                format(peripheralCalls/(os.clock() - started), 2) .. "/s"
+            )
 
             if bufferHealth < 2 * 9000 then -- Less than 2 seconds of buffer remaining
                 if curWrite + #cData > size then
@@ -188,8 +199,6 @@ local handler = {
 }
 
 http.websocketAsync(wsurl)
-startTimer = os.startTimer(5)
-repeatingTimer = os.startTimer(1)
 
 while running do
     local e = {os.pullEventRaw()}
